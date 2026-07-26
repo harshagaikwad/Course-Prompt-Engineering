838c9be6d2e73176171ce006e2d919cf0628831e
Harsha Gaikwad <142214693+harshagaikwad@users.noreply.github.com>
2026-07-26 08:26:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 // GOOGLE APPS SCRIPT WEB APP URL
 // ==========================================
 
-const GOOGLE_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbz476e53gAfbDCDPRzi68iIl6CdlM_9GV6_SrwY7fHCRdMNlNS23jrVaSpGBXh84Iah/exec";
+const GOOGLE_SCRIPT_URL ="https://script.google.com/macros/s/AKfycbxwDUhvARZ96FYptAbeWubeDrg8ciog0JuSVRhmsop7q4HIl2J0CvCTBVAc4uinbmtl/exec";
 
 
 // ==========================================
